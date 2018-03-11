08ab3fe9809f96e74d4b8c13059d0560a16ff9db
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-03-11 15:20:05 -0300

    Update cryptography Cryptonite version 0.02.2
    need fixing

--- Encrypt.py
+++ Encrypt.py
@@ -31,20 +31,24 @@ def check_next(i, s):
 
 def cryptography(s, key_var):
     c = list(s)
-    for i in range(0, len(s)):
+    i = 0
+    while i < len(s):
         if c[i] == 'a' and c[check_next(i, s)] == 's':
             c[i] = 'x'
             c[i + 1] = 'a'
-
+            i += 1
         elif c[i] == 'a' and c[check_next(i, s)] == 'r':
             c[i] = 'y'
             c[i + 1] = 'a'
+            i += 1
         elif c[i] == 'ã' and c[check_next(i, s)] == 'o':
             c[i] = 'x'
             c[i + 1] = 'y'
+            i += 1
         elif c[i] == 'ã' and not c[check_next(i, s)] == 'e':
             c[i] = 'x'
             c[i + 1] = 'z'
+            i += 1
         elif c[i] == 'A':
             c[i] = '&'
         elif c[i] == 'a' and (c[i - 1] != 'y' and c[i - 1] != 'x'):
@@ -65,14 +69,12 @@ def cryptography(s, key_var):
             c[i] = 'é'
 
         else:
-            if 65 < ord(c[i]) < 88:
-                for j in range(66, 88):
-                    if ord(c[i]) == j:
-                        c[i] = chr(ord(c[i]) - key_var)
-            else:
-                for j in range(98, 120):
-                    if ord(c[i]) == j:
-                        c[i] = chr(ord(c[i]) - key_var)
+            for j in range(32, 126):
+                if ord(c[i]) == j:
+                     c[i] = chr(ord(c[i]) - key_var)
+
+        i += 1
+
     s = ''.join([str(s) for s in c])
     return s
 
